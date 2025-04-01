390d8dbc5110a93a29f006c84ea50571dc41bfcf
mmasser95 <mmasser95@gmail.com>
2025-04-01 18:07:21 +0200

    ADD: Integrate IonFab for adding new recipes in RecetasList component

--- src/ui/components/Recetas/RecetasList.tsx
+++ src/ui/components/Recetas/RecetasList.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react"
 import { Receta } from "../../../domain/entities/receta"
 import { recetaService } from "../../../infrastructure/config"
-import { IonItem, IonLabel, IonList, } from "@ionic/react"
+import { IonFab, IonFabButton, IonIcon, IonItem, IonLabel, IonList, useIonModal, } from "@ionic/react"
+import { add } from "ionicons/icons"
+import RecetaAdd from "./RecetaAdd"
+import { OverlayEventDetail } from "@ionic/react/dist/types/components/react-component-lib/interfaces"
 
 const RecetasList: React.FC = () => {
     const [recetas, setRecetas] = useState<Receta[]>([])
@@ -14,19 +17,39 @@ const RecetasList: React.FC = () => {
         loadRecetas()
     }, [])
 
-    return (
-        <IonList>
-            {recetas.map(receta => (
-                <IonItem key={receta.id}
-                    routerLink={`/receta/${receta.id}`}
-                >
-                    <IonLabel>
-                        {receta.nombre}
-                    </IonLabel>
-                </IonItem>
-            ))
+    const [presentCreateRecipes, dismissCreateRecipes] = useIonModal(RecetaAdd, {
+        dismiss: (data: string, role: string) => dismissCreateRecipes(data, role),
+    })
+
+    const showCreateRecipesModal = () => {
+        presentCreateRecipes({
+            onWillDismiss: (e: CustomEvent<OverlayEventDetail>) => {
+                if (e.detail.role === 'confirm')
+                    console.log(`Hello, ${e.detail.data}`);
             }
-        </IonList >
+        })
+    }
+
+    return (
+        <>
+            <IonList>
+                {recetas.map(receta => (
+                    <IonItem key={receta.id}
+                        routerLink={`/receta/${receta.id}`}
+                    >
+                        <IonLabel>
+                            {receta.nombre}
+                        </IonLabel>
+                    </IonItem>
+                ))
+                }
+            </IonList >
+            <IonFab slot="fixed" vertical="bottom" horizontal="end" onClick={showCreateRecipesModal}>
+                <IonFabButton>
+                    <IonIcon icon={add} />
+                </IonFabButton>
+            </IonFab>
+        </>
     )
 }
 export default RecetasList
