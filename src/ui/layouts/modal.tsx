c528063c5d986254ecd311746259d65c61655718
mmasser95 <mmasser95@gmail.com>
2025-04-01 18:06:35 +0200

    ADD: Create ModalLayout component for consistent modal UI structure

---
+++ src/ui/layouts/modal.tsx
@@ -0,0 +1,29 @@
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonButtons, IonButton, IonIcon, IonContent } from "@ionic/react"
+import { close } from "ionicons/icons"
+
+interface Props {
+    title: string
+    children: React.ReactNode
+    dismiss: (data: string, role: string) => void
+}
+const ModalLayout: React.FC<Props> = ({ title, children, dismiss }) => {
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>{title}</IonTitle>
+                    <IonButtons slot="end">
+                        <IonButton onClick={() => dismiss("", 'cancel')} slot="icon-only">
+                            <IonIcon icon={close} />
+                        </IonButton>
+                    </IonButtons>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                {children}
+            </IonContent>
+        </IonPage>
+    )
+}
+export default ModalLayout
