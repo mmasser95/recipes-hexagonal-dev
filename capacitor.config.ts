afa5e13a4fcc1bbe7f572763f1090221fdf015b1
mmasser95 <mmasser95@gmail.com>
2025-04-01 12:13:56 +0200

    ADD: Create capacitor.config.ts for Capacitor app configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'mmasser95.receptes.app',
+  appName: 'Receptes',
+  webDir: 'build'
+};
+
+export default config;
